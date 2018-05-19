398eb37769596620e6d4ac9adaf8627932faa285
Marshall Ehlinger <mehlinger333@gmail.com>
2018-05-19 11:25:56 -0700

    Add Delete Endpoint, refactor items->artifacts

--- main.go
+++ main.go
@@ -15,17 +15,17 @@ type Artifact struct {
 }
 
 // In lieu of proper DB connection, this is our data source of record
-var items []Artifact
+var artifacts []Artifact
 
 func getArtifacts(w http.ResponseWriter, req *http.Request) {
-    json.NewEncoder(w).Encode(items)
+    json.NewEncoder(w).Encode(artifacts)
 }
 
 func getArtifact(w http.ResponseWriter, req *http.Request) {
     params := mux.Vars(req)
-    for _, item := range items {
-        if item.ID == params["id"] {
-            json.NewEncoder(w).Encode(item)
+    for _, artifact := range artifacts {
+        if artifact.ID == params["id"] {
+            json.NewEncoder(w).Encode(artifact)
             return
         }
     }
@@ -34,16 +34,27 @@ func getArtifact(w http.ResponseWriter, req *http.Request) {
 
 func createArtifact(w http.ResponseWriter, req *http.Request) {
     params := mux.Vars(req)
-    var item Artifact
-    _ = json.NewDecoder(req.Body).Decode(&item)
-    item.ID = params["id"]
-    items = append(items, item)
-    json.NewEncoder(w).Encode(items)
+    var artifact Artifact
+    _ = json.NewDecoder(req.Body).Decode(&artifact)
+    artifact.ID = params["id"]
+    artifacts = append(artifacts, artifact)
+    json.NewEncoder(w).Encode(artifacts)
+}
+
+func deleteArtifact(w http.ResponseWriter, req *http.Request) {
+    params := mux.Vars(req)
+    for index, artifact := range artifacts {
+        if artifact.ID == params["id"] {
+            artifacts = append(artifacts[:index], artifacts[index+1:]...)
+            break
+        }
+    }
+    json.NewEncoder(w).Encode(artifacts)
 }
 
 func initData() {
-    items = append(items, Artifact{"1", "+2 Boots of Flight", "Provides the user with the power of flight"})
-    items = append(items, Artifact{"2", "+7 Sword of Reckoning", "You better believe it, buster."})
+    artifacts = append(artifacts, Artifact{"1", "+2 Boots of Flight", "Provides the user with the power of flight"})
+    artifacts = append(artifacts, Artifact{"2", "+7 Sword of Reckoning", "You better believe it, buster."})
 }
 
 func main() {
@@ -52,5 +63,6 @@ func main() {
     r.HandleFunc("/artifacts", getArtifacts).Methods("GET")
     r.HandleFunc("/artifacts/{id}", getArtifact).Methods("GET")
     r.HandleFunc("/artifacts/{id}", createArtifact).Methods("POST")
+    r.HandleFunc("/artifacts/{id}", deleteArtifact).Methods("DELETE")
     log.Fatal(http.ListenAndServe(":12345",r))
 }
