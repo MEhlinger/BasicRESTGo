f5e933c395c204b9b326ef7fdf884a69ef3b54e4
Marshall Ehlinger <mehlinger333@gmail.com>
2018-05-25 12:11:17 -0700

    Update unit tests to compare payloads, not response codes

--- unit_test.go
+++ unit_test.go
@@ -5,8 +5,8 @@ import (
     "net/http"
     "net/http/httptest"
     "bytes"
-    "fmt"
     "os"
+    "strings"
 
     "github.com/gorilla/mux"
 )
@@ -14,34 +14,34 @@ import (
 var  testArtifact = []byte(`{"name":"testArtifact","detail":"This is a test artifact."}`)
 
 func TestGetArtifacts(t *testing.T) {
+    expected := `[{"id":"1","name":"+2 Boots of Flight","detail":"Provides the user with the power of flight"},{"id":"2","name":"+7 Sword of Reckoning","detail":"You better believe it, buster."}]`
     req, _ := http.NewRequest("GET", "/artifacts", nil)
     recorder := httptest.NewRecorder()
     getArtifacts(recorder, req)
-    fmt.Printf("\ngetArtifacts:\n%v\n", recorder.Body)
-    if recorder.Code != http.StatusOK {
-        t.Errorf("Status code returned was %v , expected http.StatusOK.", recorder.Code)
+    if actual := strings.TrimSpace(recorder.Body.String()); actual != expected {
+        t.Errorf("Response was:\n%v\nexpected:\n%v\n", actual, expected)
     }
 }
 
 func TestGetArtifact(t *testing.T) {
+    expected := `{"id":"2","name":"+7 Sword of Reckoning","detail":"You better believe it, buster."}`
     req, _ := http.NewRequest("GET", "artifacts/2", nil)
     req = mux.SetURLVars(req, map[string]string{"id":"2"})
     recorder := httptest.NewRecorder()
     getArtifact(recorder, req)
-    fmt.Printf("\ngetArtifact:\n%v\n", recorder.Body)
-    if recorder.Code != http.StatusOK {
-        t.Errorf("Status code returned was %v, expected http.StatusOK.", recorder.Code)
+    if actual := strings.TrimSpace(recorder.Body.String()); actual != expected {
+        t.Errorf("Response was:\n%v\nexpected:\n%v\n", actual, expected)
     }
 }
 
 func TestCreateArtifact(t *testing.T) {
+    expected := `[{"id":"1","name":"+2 Boots of Flight","detail":"Provides the user with the power of flight"},{"id":"2","name":"+7 Sword of Reckoning","detail":"You better believe it, buster."},{"id":"3","name":"testArtifact","detail":"This is a test artifact."}]`
     req, _ := http.NewRequest("POST", "/artifacts/3", bytes.NewBuffer(testArtifact))
     req = mux.SetURLVars(req, map[string]string{"id":"3"})
     recorder := httptest.NewRecorder()
     createArtifact(recorder, req)
-    fmt.Printf("\ncreateArtifact:\n%v\n", recorder.Body)
-    if recorder.Code != http.StatusOK {
-        t.Errorf("Status code returned was %v , expected http.StatusOK.", recorder.Code)
+    if actual := strings.TrimSpace(recorder.Body.String()); actual != expected {
+        t.Errorf("Response was:\n%v\nexpected:\n%v\n", actual, expected)
     }
 }
 
@@ -50,26 +50,24 @@ func TestCreateArtifactDuplicateID(t *testing.T) {
     req = mux.SetURLVars(req, map[string]string{"id":"3"})
     recorder := httptest.NewRecorder()
     createArtifact(recorder, req)
-    fmt.Printf("\ncreateArtifact duplicate:\n%v\n", recorder.Body)
     if recorder.Code != http.StatusBadRequest {
         t.Errorf("Status code returned was %v, expected 400", recorder.Code)
     }
 }
 
 func TestDeleteArtifact(t *testing.T) {
+    expected := `[{"id":"1","name":"+2 Boots of Flight","detail":"Provides the user with the power of flight"},{"id":"2","name":"+7 Sword of Reckoning","detail":"You better believe it, buster."}]`
     req, _ := http.NewRequest("DELETE", "/artifacts/3", nil)
     req = mux.SetURLVars(req, map[string]string{"id":"3"})
     recorder := httptest.NewRecorder()
     deleteArtifact(recorder, req)
-    fmt.Printf("\ndeleteArtifact:\n%v\n", recorder.Body)
-    if recorder.Code != http.StatusOK {
-        t.Errorf("Status code from delete attempt was %v, expected http.StatusOK", recorder.Code)
+    if actual := strings.TrimSpace(recorder.Body.String()); actual != expected {
+        t.Errorf("Response was:\n%v\nexpected:\n%v\n", actual, expected)
     }
 }
 
 func setup() {
     initData()
-    fmt.Printf("\nInitial Data:\n%v\n", artifacts)
 }
 
 func TestMain(m *testing.M) {
